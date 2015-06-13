27840ac75b1dd5deb3cfa903007eb65b967de635
Dana Burkart <dana.burkart@gmail.com>
2015-06-12 22:15:26 -0700

    Make check-sieve a little more user-friendly.

--- sieve.cc
+++ sieve.cc
@@ -1,19 +1,18 @@
 #include <iostream>
 #include "sieve_driver.hh"
 
-int
-main (int argc, char *argv[])
-{
-  int res = 0;
-  sieve_driver driver;
-  for (int i = 1; i < argc; ++i)
-    if (argv[i] == std::string ("-p"))
-      driver.trace_parsing = true;
-    else if (argv[i] == std::string ("-s"))
-      driver.trace_scanning = true;
-    else if (!driver.parse_file(argv[i]))
-      std::cout << driver.result << std::endl;
-    else
-      res = 1;
-  return res;
+
+int main( int argc, char *argv[] ) {
+    int result = 0;
+    
+    sieve_driver driver;
+    for (int i = 1; i < argc; ++i) {
+        if ( !driver.parse_file(argv[i]) ) {
+            std::cout << "No errors found!" << std::endl;
+        } else {
+            result = 1;
+        }
+    }
+    
+    return result;
 }

3655fdda768fc60af692ea8e63ef24243ed60a66
Dana Burkart <dana.burkart@gmail.com>
2015-06-13 01:57:48 -0700

    Add constructor for parse_result struct, and include location.hh

--- src/checksieve.h
+++ src/checksieve.h
@@ -2,16 +2,16 @@
 #define __CHECKSIEVE_H__
 
 #include <string>
-#include "sieve_driver.hh"
-
-namespace yy {
-    class location;
-}
+#include "location.hh"
 
 struct parse_result {
     int status;
     yy::location location;
     std::string error;
+    
+    parse_result() {
+        status = 0;
+    }
 };
 
 struct parse_result sieve_parse_file( const char *filename );

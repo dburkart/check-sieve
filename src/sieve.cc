3d3bcee86f99dd8b377b93e689831f06c104579e
Dana Burkart <dana.burkart@gmail.com>
2015-07-17 18:59:58 -0700

    Error out if we can't find one of the files passed in.
    Resolves issue #12

--- src/sieve.cc
+++ src/sieve.cc
@@ -1,12 +1,23 @@
 #include <iostream>
+#include <fstream>
 #include "sieve_driver.hh"
 
+bool file_exists(const char *filename) {
+    std::ifstream file(filename);
+    return file;
+}
 
 int main( int argc, char *argv[] ) {
     int result = 0;
     
     sieve_driver driver;
     for (int i = 1; i < argc; ++i) {
+        if (!file_exists(argv[i])) {
+            std::cout << "Could not find file \"" << argv[i] << "\"." << std::endl;
+            result = 2;
+            break;
+        }
+        
         if ( !driver.parse_file(argv[i]) ) {
             std::cout << "No errors found!" << std::endl;
         } else {

70c96edf2c4f53045df30f05a3878cfe90f4f013
Dana Burkart <dana.burkart@gmail.com>
2015-08-10 17:57:01 -0700

    Implement "Copying without side effects" (RFC 3894).
    This resolves issue #15.

---
+++ test/3894/basic_test.py
@@ -0,0 +1,49 @@
+import sys
+sys.path.append('./')
+
+import unittest
+import checksieve
+
+class TestBasic(unittest.TestCase):
+    
+    def test_copy_fileinto (self):
+        sieve = '''
+            require ["copy", "fileinto"];
+            
+            if header :matches "Subject" "*" {
+                fileinto :copy "All Mail";
+            }
+            
+            keep;
+            stop;
+        '''
+        self.assertFalse(checksieve.parse_string(sieve, False))
+    
+    def test_copy_redirect (self):
+        sieve = '''
+            require ["copy"];
+            
+            if header :matches "Subject" "*" {
+                redirect :copy "foo@example.com";
+            }
+            
+            keep;
+            stop;
+        '''
+        self.assertFalse(checksieve.parse_string(sieve, False))
+    
+    def test_copy_no_require (self):
+        sieve = '''
+            require ["fileinto"];
+            
+            if header :matches "Subject" "*" {
+                fileinto :copy "All Mail";
+            }
+            
+            keep;
+            stop;
+        '''
+        self.assertTrue(checksieve.parse_string(sieve, True))
+
+if __name__ == '__main__':
+    unittest.main()

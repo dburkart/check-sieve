8df4736fd9e8ac946d04c5b5f9c7b790cbf9f478
Stuart Montgomery <smontgomery@apple.com>
2015-09-08 18:32:12 -0700

    Add tests of :copy before :flags

--- test/5232/fileinto_test.py
+++ test/5232/fileinto_test.py
@@ -34,10 +34,24 @@ class TestFileinto(unittest.TestCase):
         '''
         self.assertFalse(checksieve.parse_string(sieve, False))
     
+    def test_fileinto_with_copy_and_flag(self):
+        sieve = '''
+        require ["fileinto", "imap4flags", "copy"];
+        fileinto :copy :flags "\\\\Seen" "Mailbox";
+        '''
+        self.assertFalse(checksieve.parse_string(sieve, False))
+    
     def test_fileinto_with_flag_without_arguments_and_copy(self):
         sieve = '''
         require ["fileinto", "imap4flags", "copy"];
         fileinto :flags :copy "Mailbox";
+        '''
+        self.assertTrue(checksieve.parse_string(sieve, True))
+    
+    def test_fileinto_with_copy_and_flag_without_arguments(self):
+        sieve = '''
+        require ["fileinto", "imap4flags", "copy"];
+        fileinto :copy :flags "Mailbox";
         '''
         self.assertTrue(checksieve.parse_string(sieve, True))
     

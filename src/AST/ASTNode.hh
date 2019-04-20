97ede941c3c71a214b33e8780292c663e322f48b
Dana Burkart <dana.burkart@gmail.com>
2019-04-19 23:17:03 -0700

    Implement validation for :comparator.

--- src/AST/ASTNode.hh
+++ src/AST/ASTNode.hh
@@ -15,21 +15,42 @@ class ASTNode {
 public:
     ASTNode() : _children() {}
     ASTNode(const yy::location &location)
-        : _children()
+        : _parent(NULL)
+        , _children()
         , _location(location.begin, location.end) {}
 
     virtual void accept(ASTVisitor& visitor) =0;
     
     const std::vector<ASTNode *> &children() const { return this->_children; }
+    const ASTNode *nextChild(const ASTNode *child) const {
+        const ASTNode *next = NULL;
+        std::vector<sieve::ASTNode *> children = _children;
+        for (std::vector<ASTNode *>::iterator it = children.begin(); it != children.end(); ++it) {
+            if (child == *it) {
+                ++it;
+                if (it == children.end())
+                    break;
+                next = *it;
+            }
+        }
+        return next;
+    };
     
-    void push(ASTNode *child) { this->_children.push_back(child); } 
+    const ASTNode *parent() const { return _parent; }
+    void setParent(ASTNode *parent) { _parent = parent; }
+
+    void push(ASTNode *child) { child->setParent(this); this->_children.push_back(child); }
     void push(std::vector<ASTNode *> children) { 
+        for (std::vector<ASTNode *>::iterator it = children.begin(); it != children.end(); ++it) {
+            (*it)->setParent(this);
+        }
         this->_children.insert(this->_children.end(), children.begin(), children.end());
     }
 
     yy::location location() const { return this->_location; }
 
 private:
+    ASTNode *_parent;
     std::vector<ASTNode *> _children;
     yy::location _location;
 };
--- src/AST/Validation/Tag.cc
+++ src/AST/Validation/Tag.cc
@@ -1,9 +1,31 @@
+#include "ASTString.hh"
 #include "Tag.hh"
 
 namespace sieve {
 
+bool validateComparator(const ASTNode *node) {
+    const ASTTag *tag = dynamic_cast<const ASTTag*>(node);
+    const ASTNode *parent = tag->parent();
+    const ASTNode *next = parent->nextChild(tag);
+
+    if (next == NULL) {
+        return false;
+    }
+
+    const ASTString *nextString = dynamic_cast<const ASTString *>(next) ;
+    if (nextString == NULL) {
+        return false;
+    }
+
+    // Validate comparator string
+
+    return true;
+}
+
 Tag::Tag() {
-    
+    _usage_map[":comparator"] = ":comparator string";
+
+    _validation_fn_map[":comparator"] = &validateComparator;
 }
 
 bool Tag::validate(const ASTTag *tag) {

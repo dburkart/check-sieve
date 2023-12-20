d8f2a182e6bb7e6ba14b82a75effa2dc76e5e078
Dana Burkart <dana.burkart@gmail.com>
2023-12-20 10:18:34 -0800

    Replace C++11 with C++17 in all the places.

--- test/setup.py
+++ test/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 from distutils.sysconfig import get_python_inc
 
 module = Extension('checksieve',
-                    extra_compile_args = ['-std=c++11'],
+                    extra_compile_args = ['-std=c++17'],
                     sources = ['src/python.cc'],
                     include_dirs = ['src', 'src/AST', 'gen', get_python_inc(), get_python_inc(plat_specific=True)],
                     libraries = ['checksieve'],

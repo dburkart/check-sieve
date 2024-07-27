795cfb111a76d8dcb23674ad576666299364c011
Dana Burkart <dana.burkart@gmail.com>
2024-07-27 13:54:39 -0700

    Attempt to remove distutils to fix the build on python 3.12.x (#66)

--- test/setup.py
+++ test/setup.py
@@ -1,10 +1,9 @@
-from distutils.core import setup, Extension
-from distutils.sysconfig import get_python_inc
+from setuptools import Extension, setup
 
 module = Extension('checksieve',
                     extra_compile_args = ['-std=c++17'],
                     sources = ['src/python.cc'],
-                    include_dirs = ['src', 'src/AST', 'gen', get_python_inc(), get_python_inc(plat_specific=True)],
+                    include_dirs = ['src', 'src/AST', 'gen'],
                     libraries = ['checksieve'],
                     library_dirs = ['./'] );
 
